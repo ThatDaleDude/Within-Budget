cf7e49dac374debcac81c4b6006f53be727379d0
Dale <dalegdawg@hotmail.co.uk>
2025-09-28 17:23:59 +0100

    Creates component for display api errors on the auth pages

---
+++ WithinBudget/Components/ApiErrorCard.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+using WithinBudget.Shared;
+
+namespace WithinBudget.Components;
+
+public partial class ApiErrorCard : ComponentBase
+{
+    [Parameter] public ApiError ApiError { get; set; } = new();
+}
